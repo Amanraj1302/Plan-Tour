58057aae9e4be1a249587dd580a37af68f69f59d
Amanraj1302 <amanraaj239@gmail.com>
2024-06-15 20:06:43 +0530

    update

--- src/components/Tours.jsx
+++ src/components/Tours.jsx
@@ -4,7 +4,7 @@ function Tours({ tours, removeTour }) {
     return (
         <div className="container">
             <div>
-                <h2 className="title">Trip with Nitin</h2>
+                <h2 className="title">Trip with Aman</h2>
             </div>
             <div className="cards">
                 {
